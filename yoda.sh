f29607a4824aa4644f27dc9ed909bbd91a57d403
jakecomp <jakobvalen9@gmail.com>
2022-03-12 15:01:26 -0800

    Add domain grep from url and update domain_info function

--- yoda.sh
+++ yoda.sh
@@ -4,9 +4,10 @@ echo ""
 echo "See through you...we can" 
 echo ""
 
-cat yoda_logo.txt
+cat yoda_logo.txt 
+echo ""
 
-while getopts i:d:e:u flag 
+while getopts i:d:e:u:* flag 
 
 do 
 
@@ -18,14 +19,16 @@ do
             ;;
         e) email=${OPTARG}
             ;; 
-        u) url=${OPTARG} 
+        u) url=${OPTARG}
             ;; 
         *) exit 0;; 
 
 
     esac 
 
-done   
+done  
+
+echo "MY URL IS $url"
 
 find_ip_info() {  
 
@@ -45,26 +48,66 @@ find_ip_info() {
     echo "-- $1 FOUND PHONE NUMBERS" 
     echo "" 
     $whoisinfo | grep 'Org' | grep -v ":$" | grep 'Phone'
-    echo ""
+    echo "" 
+
+    if [ $2 = true ]
+    then 
+        echo "NO DOMAIN GIVEN" 
+    fi
 }
 
-find_domain_info() {  
+find_domain_info() {
 
-    whoisinfo=$"whois $1"
+    whoisinfo=$"whois $1" 
 
-    echo "--$1 ADMIN INFO" 
+    echo "--$1 IMPORTANT DATES" 
+    echo ""
+    $whoisinfo | grep -v ":$" | grep "Date" 
+    echo ""
+
+    echo "--$1 FOUND NAMES" 
     echo "" 
-    $whoisinfo | grep 'Admin' | grep -v ":$" | grep 'Name\|City\|State/Province\|Country\|Email\|Phone\|Organization'  
-    echo ""  
+    $whoisinfo | grep -v ":$" | grep 'Name\|Organization' | grep -v "Server"
+    echo "" 
+
+    echo "--$1 FOUND EMAILS" 
+    echo "" 
+    $whoisinfo | grep -v ":$" | grep "Email" 
+    echo "" 
+
+    echo "--$1 FOUND PHONE NUMBERS"
+    echo ""
+    $whoisinfo | grep -v ":$" | grep 'Phone' 
+    echo "" 
+
+    echo "--$1 FOUND GEOGRAPHICAL LOCATIONS" 
+    echo "" 
+    $whoisinfo | grep -v ":$" | grep 'Country\|State/Province\|City' 
+    echo ""
     
     declare -a name_servers=($($whoisinfo | grep -v ":$" | sed -n -e 's/^.*Name Server://p'))
 
-    echo "--$1 NAME SERVER INFO" 
+    echo "--$1 FOUND NAME SERVERS" 
     echo ""
     for i in "${name_servers[@]}" 
     do 
         echo "Name Server: $i , IP: $(dig +short $i)"
-    done
+    done 
+
+    echo ""  
+    echo "--$1 FOUND INFO ON NAME SERVERS" 
+    echo ""
+    for i in "${name_servers[@]}" 
+    do 
+        find_ip_info $(dig +short $i) false 
+    done 
+
+    echo "" 
+    echo "--$1 TRACE ROUTE" 
+    echo "" 
+    traceroute $1
+
+
 }
 
 
@@ -75,8 +118,23 @@ then
 
     find_domain_info $domain 
 
+    if ! [ -z $ip ] 
+    then 
+
+        find_ip_info $ip false 
+
+    fi
+
 elif ! [ -z $ip ] 
 then 
 
-    find_ip_info $ip
+    find_ip_info $ip true 
+
+elif ! [ -z $url ] 
+then  
+
+    echo "WE HAVE A URL!"
+
+    domain=$( echo "$url" | sed -n -e 's/^.*www.//p')
+    echo "DOMAIN IS $domain"
 fi
